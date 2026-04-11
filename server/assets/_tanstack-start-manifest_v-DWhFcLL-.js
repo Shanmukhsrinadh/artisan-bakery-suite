8291e2c5dce00300d392235881fb742fdbf0e747
Shanmukhsrinadh <shimachalamshanmuk60297@gmail.com>
2026-04-11 17:01:31 +0530

    Updates

--- server/assets/_tanstack-start-manifest_v-DWhFcLL-.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ "routes": { "__root__": { "filePath": "C:/Users/shima/OneDrive/Desktop/shannu/artisan-bakery-suite/src/routes/__root.tsx", "children": ["/"], "preloads": ["/artisan-bakery-suite/assets/index-BJMw22lq.js"] }, "/": { "filePath": "C:/Users/shima/OneDrive/Desktop/shannu/artisan-bakery-suite/src/routes/index.tsx", "preloads": ["/artisan-bakery-suite/assets/index-mBuKU6UM.js"] } }, "clientEntry": "/artisan-bakery-suite/assets/index-BJMw22lq.js" });
-export {
-  tsrStartManifest
-};
